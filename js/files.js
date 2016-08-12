5e44853f8ffc8190ba70f5adcf11497e219f22cb
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2016-08-12 15:16:52 -0400

    Adds better demo clip of Smokey.

--- js/files.js
+++ js/files.js
@@ -1,23 +1,24 @@
 VideoFiles = 
 [
-  {
-     "name" : "smokey",
-     "_versionMajor" : "0",
-     "_versionMinor" : "1",
-     "depthFocalLength" : {
-        "x" : 365.2185974121094,
-        "y" : 365.2185974121094
-     },
-     "depthImageSize" : {
-        "x" : 512,
-        "y" : 424
-     },
-     "depthPrincipalPoint" : {
-        "x" : 257.6430969238281,
-        "y" : 209.3957977294922
-     },
-     "farClip" : 1204.192016601562,
-     "format" : "perpixel",
-     "nearClip" : 0
-  }
+{
+    "name" : "smokey",
+   "_versionMajor" : "0",
+   "_versionMinor" : "1",
+   "depthFocalLength" : {
+      "x" : 365.2185974121094,
+      "y" : 365.2185974121094
+   },
+   "depthImageSize" : {
+      "x" : 512,
+      "y" : 424
+   },
+   "depthPrincipalPoint" : {
+      "x" : 257.6430969238281,
+      "y" : 209.3957977294922
+   },
+   "farClip" : 1204.192016601562,
+   "format" : "perpixel",
+   "nearClip" : 434.166748046875
+}
+
 ]
